f7322c0b6f357d3b641a0f46f0fcdb20eda9b67d
ElinorG11 <48283282+ElinorG11@users.noreply.github.com>
2021-06-16 12:47:47 +0300

    Initial commit

---
+++ ID3.py
@@ -0,0 +1,11 @@
+# The file was empty. I've created signatures of the required classes\methods from the
+# pdf for convenience.
+class ID3:
+    def __init__(self):
+        raise NotImplementedError
+
+    def fit_predict(self, train, test):
+        raise NotImplementedError
+
+    def experiment(self):
+        raise NotImplementedError
---
+++ PersonalizedID3.py
@@ -0,0 +1,8 @@
+# The file was empty. I've created signatures of the required classes\methods from the
+# pdf for convenience.
+class PersonalizedID3:
+    def __init__(self):
+        raise NotImplementedError
+
+    def fit_predict(self, train, test):
+        raise NotImplementedError
