86bdaa78cacf4b6f4e2dddb3ff3ade2bc4aae549
ElinorG11 <48283282+ElinorG11@users.noreply.github.com>
2021-06-17 08:21:33 +0300

    Initial commit

--- ID3.py
+++ ID3.py
@@ -1,10 +1,43 @@
 # The file was empty. I've created signatures of the required classes\methods from the
 # pdf for convenience.
+class Tree:
+    """
+    Tree class creates the ID3 tree
+    """
+    def __init__(self,data=None):
+        """
+        Init function generates the tree.
+        :param data: ?
+        :type data: ?
+        """
+        self.feature = None
+        self.slicing_val = None
+        self.left = None
+        self.right = None
+        self.data = data
+        self.diagnosis = None
+
+        # check if we reached a leaf and whether it is homogenous
+        leaf, diagnosis = self.check_homogenous_leaves()
+
+    def check_homogenous_leaves(self):
+        data = []
+        data.index
+
+
 class ID3:
     def __init__(self):
         raise NotImplementedError
 
     def fit_predict(self, train, test):
+        """
+        A classifire the utilizes the ID3 tree to fit and predict.
+        fitting the data into ID3 tree and predicts the diagnosis for data set x.
+        computes accuracy and loss for y.
+        :param train: dataset
+        :param test:
+        :return:
+        """
         raise NotImplementedError
 
     def experiment(self):
